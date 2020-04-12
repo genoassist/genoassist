7c3b4379eaf6d37495590624707c2c602a0a5bfc
FlaviuVadan <flaviuvadan@gmail.com>
2020-04-11 20:08:40 -0600

    Add TODOs to assembler.go to implement finding the container to be launched

--- src/slave/components/assembler/assembler.go
+++ src/slave/components/assembler/assembler.go
@@ -2,8 +2,10 @@
 package assembler
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 
 	"github.com/genomagic/src/slave/components"
@@ -18,16 +20,26 @@ type asmbler struct {
 }
 
 // NewAssembler returns a new assembler for the specified file
+// TODO: this needs to take in an assembler name
 func NewAssembler(fnm string) (components.Component, error) {
 	a := &asmbler{
 		fileName: fnm,
 	}
-	if cli, err := client.NewEnvClient(); err != nil {
+	cli, err := client.NewEnvClient()
+	if err != nil {
 		return nil, fmt.Errorf("failed to initialize Docker client, err: %v", err)
 	} else {
 		a.dClient = cli
 	}
-	containers, err := 1, nil
+	images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get available Docker containers, err: %v", err)
+	}
+	// need to find whether the megahit container is available
+	// TODO: this needs to find the given assembler container and call it with appropriate params
+	for _, im := range images {
+		fmt.Printf("%s %v\n", im.ID, im.RepoTags)
+	}
 	return a, nil
 }
 
--- src/slave/components/assembler/assembler_test.go
+++ src/slave/components/assembler/assembler_test.go
@@ -1,1 +1,17 @@
 package assembler
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewAssembler(t *testing.T) {
+	asm, err := NewAssembler("hello")
+	if err != nil {
+		fmt.Printf("err in new")
+		return
+	}
+	if err := asm.Process(); err != nil {
+		fmt.Println("err")
+	}
+}
