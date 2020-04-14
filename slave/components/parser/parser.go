16ebe7684d2bec478155cb9d0ccc14943a13ce82
FlaviuVadan <flaviuvadan@gmail.com>
2020-04-14 16:46:01 -0600

    Add some simple parser tests

--- slave/components/parser/parser.go
+++ slave/components/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/genomagic/slave/components"
 )
 
@@ -13,6 +15,9 @@ type prser struct {
 // NewParser creates and returns a new parser struct
 // TODO: parser needs to take in an additional param for the assembler results to process, left as _ to satisfy interface
 func NewParser(filePath, _ string) (components.Component, error) {
+	if filePath == "" {
+		return nil, fmt.Errorf("cannot initialize parser with an empty file path")
+	}
 	return &prser{
 		filePath: filePath,
 	}, nil
