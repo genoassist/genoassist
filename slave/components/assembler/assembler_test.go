81c77e849581c06aa14efbe4ffcb09437c10c115
FlaviuVadan <flaviuvadan@gmail.com>
2020-04-14 17:05:43 -0600

    Comment out local assembler test

--- slave/components/assembler/assembler_test.go
+++ slave/components/assembler/assembler_test.go
@@ -1,21 +1,18 @@
 package assembler
 
 import (
-	"fmt"
 	"testing"
-
-	"github.com/genomagic/constants"
 )
 
 // manually testing New and Process for now
 // TODO: figure out a way to test the assembler
 func TestNewAssembler(t *testing.T) {
-	a, err := NewAssembler("", constants.MegaHit)
-	if err != nil {
-		fmt.Printf(fmt.Sprintf("err: %v", err))
-	}
-	err = a.Process()
-	if err != nil {
-		fmt.Printf("err: %v\n", err)
-	}
+	//a, err := NewAssembler("", constants.MegaHit)
+	//if err != nil {
+	//	fmt.Printf(fmt.Sprintf("err: %v", err))
+	//}
+	//err = a.Process()
+	//if err != nil {
+	//	fmt.Printf("err: %v\n", err)
+	//}
 }
