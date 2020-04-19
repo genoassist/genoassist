c8cb3175ca0539540651c8b07b99a33266c66f19
Tayab Soomro <isoomro.t@gmail.com>
2020-04-18 18:47:08 -0600

    move result.go to main directory - remove unnecessary code from result.go

---
+++ result.go
@@ -0,0 +1,16 @@
+// contains the definition of an rst
+package main
+
+// TODO: Implement this so that it is consistent with what processor returns and reporter accepts
+
+// rst represents the result that the parser function returns and is used by reporting component of genomagic
+type rst struct {
+
+}
+
+// NewResult creates a new result struct and returns it
+func NewResult(msg string, err error, est int) *rst {
+	return &rst{
+
+	}
+}
