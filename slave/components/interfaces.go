13525139d684b0388a030dcfff5290a322235980
Tayab Soomro <isoomro.t@gmail.com>
2020-04-18 20:28:56 -0600

    add result.Result object as return object for Process method

--- slave/components/interfaces.go
+++ slave/components/interfaces.go
@@ -1,8 +1,10 @@
 // defines interfaces that have to be satisfied by components
 package components
 
+import "github.com/genomagic/result"
+
 // Component defines the operations that apply to slave components such as assemblers and parsers
 type Component interface {
 	// Process performs the work associated with this worker
-	Process() error
+	Process() (result.Result,error)
 }
