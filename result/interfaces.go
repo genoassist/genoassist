0e0e9e1399ba77e7e34889167466230a3a167897
Tayab Soomro <isoomro.t@gmail.com>
2020-04-19 22:37:44 -0600

    remove slave interface from result package

--- result/interfaces.go
+++ result/interfaces.go
@@ -12,9 +12,3 @@ type Result interface {
 	// GetExitStatusCode returns the status code that was returned upon assembly completion
 	GetExitStatusCode()
 }
-
-// Slave defines the operations that are accessible on a slave
-type Slave interface {
-	// Process performs the work that's dictated by the master
-	Process() (Result,error)
-}
