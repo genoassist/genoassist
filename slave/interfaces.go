8445d16ea71bf3888b041aec7a9d08ba0ddb3ece
Tayab Soomro <isoomro.t@gmail.com>
2020-04-19 22:40:35 -0600

    add slave interface into slave package

--- slave/interfaces.go
+++ slave/interfaces.go
@@ -1,1 +1,10 @@
 package slave
+
+import "github.com/genomagic/result"
+
+// Slave defines the operations that are accessible on a slave
+type Slave interface {
+	// Process performs the work that's dictated by the master
+	Process() (*result.Result,error)
+}
+
