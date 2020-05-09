a267d3912278a657c7a2f6de7ffe38e7dafaeae3
flaviuvadan <flaviuvadan@gmail.com>
2020-05-09 15:30:29 -0600

    Add additional documentation to constants
    Add conditions to available assemblers to specify whether there are any conditions to be satisfied ahead of assembly
    
    Add CreateDir condition for Abyss
    
    Co-authored-by: Tayab <isoomro.t@gmail.com>

--- constants/util.go
+++ constants/util.go
@@ -1,52 +1,88 @@
 // constants holds all the constants shared between packages
 package constants
 
-import "path"
+import "C"
+import (
+	"fmt"
+	"path"
+)
 
 const (
+	// GenoMagic represents the name of this program
 	GenoMagic = "genomagic"
 
-	BaseOut  = "/output"
+	// BaseOut is the base directory used as the output, mounted by Docker
+	BaseOut = "/output"
+
+	// RawSeqIn is the mapping of the user-specified reads file to the Docker mount file
 	RawSeqIn = "/raw_sequence_input.fastq"
 
+	// MegaHit specific constants
 	MegaHit    = "megahit"
 	MegaHitOut = GenoMagic + "_megahit_out"
 
+	// Abyss specific constants
 	Abyss    = "abyss"
 	AbyssOut = GenoMagic + "_abyss_out"
+
+	// CreateDir informs the condition function to create a directory for an assembler
+	CreateDir = "CreateDir"
 )
 
-// getAssemblerCommand returns the Docker container command associated with an assembler
-type getAssemblerCommand func() []string
+type (
+	// getAssemblerCommand returns the Docker container command associated with an assembler
+	getAssemblerCommand func() []string
 
-// assemblerDetails holds the details of each assembler
-type AssemblerDetails struct {
-	Name             string              // assembler name
-	DHubURL          string              // DockerHub url of the assembler image
-	OutputDir        string              // output directory where to read assembled sequences from, no longer bound to Docker
-	AssemblyFileName string              // name of the resulting assembly file
-	Comm             getAssemblerCommand // function to return the Docker command of the assembler
-}
+	// Condition that is run by the condition command
+	Condition string
 
-// AvailableAssemblers defines the structs of currently integrated assemblers
-var AvailableAssemblers = map[string]*AssemblerDetails{
-	MegaHit: {
-		Name:             MegaHit,
-		DHubURL:          "docker.io/vout/megahit", // https://github.com/voutcn/megahit
-		OutputDir:        MegaHitOut,
-		AssemblyFileName: "/final.contigs.fa",
-		Comm: func() []string {
-			// NOTE: input filePath and outPath are mapped to Docker mounts during creation (slave/components/assembler/assembler.go:87)
-			return []string{"-r", RawSeqIn, "-o", path.Join(BaseOut, MegaHitOut)}
+	// AssemblerDetails holds the details of each assembler
+	AssemblerDetails struct {
+		Name              string              // assembler name
+		DHubURL           string              // DockerHub url of the assembler image
+		OutputDir         string              // output directory where to read assembled sequences from, no longer bound to Docker
+		AssemblyFileName  string              // name of the resulting assembly file
+		Comm              getAssemblerCommand // function to return the Docker command of the assembler
+		ConditionsPresent bool                // whether there are any special functions to run
+		Conditions        []Condition         // list of conditions to run before an assembler runs
+	}
+)
+
+var (
+	// AvailableAssemblers defines the structs of currently integrated assemblers
+	AvailableAssemblers = map[string]*AssemblerDetails{
+		MegaHit: {
+			Name:             MegaHit,
+			DHubURL:          "docker.io/vout/megahit", // https://github.com/voutcn/megahit
+			OutputDir:        MegaHitOut,
+			AssemblyFileName: "/final.contigs.fa",
+			Comm: func() []string {
+				// NOTE: input filePath and outPath are mapped to Docker mounts during creation (slave/components/assembler/assembler.go:87)
+				return []string{
+					"-r", RawSeqIn,
+					"-o", path.Join(BaseOut, MegaHitOut),
+				}
+			},
+			ConditionsPresent: false,
 		},
-	},
-	Abyss: {
-		Name:             Abyss,
-		DHubURL:          "docker.io/bcgsc/abyss", // https://github.com/bcgsc/abyss
-		OutputDir:        AbyssOut,
-		AssemblyFileName: "/final.contigs.fa",
-		Comm: func() []string {
-			return []string{""}
+		Abyss: {
+			Name:             Abyss,
+			DHubURL:          "docker.io/bcgsc/abyss", // https://github.com/bcgsc/abyss
+			OutputDir:        AbyssOut,
+			AssemblyFileName: "final-contigs.fa",
+			Comm: func() []string {
+				return []string{
+					`k=25`,
+					`name=final`,
+					fmt.Sprintf("in='%s'", RawSeqIn),
+					fmt.Sprintf("--directory=%s", path.Join(BaseOut, AbyssOut)),
+					"contigs",
+				}
+			},
+			ConditionsPresent: true,
+			Conditions: []Condition{
+				CreateDir,
+			},
 		},
-	},
-}
+	}
+)
