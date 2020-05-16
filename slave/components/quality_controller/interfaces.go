bb36e4a78f70dcce63e96644230507ef18e702b3
flaviuvadan <flaviuvadan@gmail.com>
2020-05-16 17:23:08 -0600

    Document the Controller interface

--- slave/components/quality_controller/interfaces.go
+++ slave/components/quality_controller/interfaces.go
@@ -1,6 +1,9 @@
 package quality_controller
 
-// Controller represents the
+// Controller represents the interface of the quality control process
 type Controller interface {
+	// Process performs the work defined by a specific controller implementation. Since processes occurs on
+	// sequence files, the paths to the newly generated quality controlled files is returned, along with
+	// and error, in case there is one
 	Process() (string, error)
 }
