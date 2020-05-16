ed3b5078e926bdb6aab609da429785a6b80782c3
flaviuvadan <flaviuvadan@gmail.com>
2020-05-16 16:25:10 -0600

    Expand the adapter trimming constructor to take in the necessary docker cli and the GenoMagic config so it can be passed by the quality controller

--- slave/components/quality_controller/adapter_trimming.go
+++ slave/components/quality_controller/adapter_trimming.go
@@ -1,12 +1,28 @@
 package quality_controller
 
-type AdapterTrimming struct {
+import (
+	"github.com/docker/docker/client"
+
+	"github.com/genomagic/config_parser"
+)
+
+// adapterTrimming is the struct representation of the adapter trimming process
+type adapterTrimming struct {
+	// dockerCLI is used for launching a Docker container that perform adapter trimming
+	dockerCLI *client.Client
+	// genoConfig is the GenoMagic global configuration
+	genoConfig *config_parser.Config
 }
 
-func NewAdapterTrimming() Controller {
-	return &AdapterTrimming{}
+// NewAdapterTrimming constructs and returns a new instance of adapterTrimming, which implements the Controller interface
+func NewAdapterTrimming(d *client.Client, c *config_parser.Config) Controller {
+	return &adapterTrimming{
+		dockerCLI:  d,
+		genoConfig: c,
+	}
 }
 
-func (a *AdapterTrimming) Process() (string, error) {
+// Process performs the adapter trimming process by launching a Docker container that uses Trimmomatic
+func (a *adapterTrimming) Process() (string, error) {
 	return "", nil
 }
