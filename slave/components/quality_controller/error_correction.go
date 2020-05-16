be4a786812f0e3cf5887c7814c5fad57319a0eea
flaviuvadan <flaviuvadan@gmail.com>
2020-05-16 16:56:19 -0600

    Implement a mock of the error correction process

--- slave/components/quality_controller/error_correction.go
+++ slave/components/quality_controller/error_correction.go
@@ -1,11 +1,27 @@
 package quality_controller
 
-type ErrorCorrection struct{}
+import (
+	"github.com/docker/docker/client"
 
-func NewErrorCorrection() Controller {
-	return &ErrorCorrection{}
+	"github.com/genomagic/config_parser"
+)
+
+// errorCorrection is a representation of the error correction process
+type errorCorrection struct {
+	// dockerCLI is used for launching a Docker container that perform adapter trimming
+	dockerCLI *client.Client
+	// config is the GenoMagic global configuration
+	config *config_parser.Config
+	// toDecontaminate represents the path to the file to decontaminate
+	toDecontaminate string
 }
 
-func (e *ErrorCorrection) Process() (string, error) {
+// NewErrorCorrection constructs and returns an errorCorrection struct, which implements the Controller interface
+func NewErrorCorrection(dockerCli *client.Client, config *config_parser.Config, fileToDecontaminate string) Controller {
+	return &errorCorrection{}
+}
+
+// Process performs the error correction process
+func (e *errorCorrection) Process() (string, error) {
 	return "", nil
 }
