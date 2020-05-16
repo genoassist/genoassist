0c44ae183c874be6df09164ec805abfdee180b8f
flaviuvadan <flaviuvadan@gmail.com>
2020-05-16 15:57:38 -0600

    Implement a mock Decontamination Controller interface

--- slave/components/quality_controller/decontamination.go
+++ slave/components/quality_controller/decontamination.go
@@ -1,1 +1,11 @@
 package quality_controller
+
+type Decontamination struct{}
+
+func NewDecontamination() Controller {
+	return &Decontamination{}
+}
+
+func (d *Decontamination) Process() (string, error) {
+	return "", nil
+}
