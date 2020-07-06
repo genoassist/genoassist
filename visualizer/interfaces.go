8c0d5a13910484e7f17d6b18707bb918e42962a7
flaviuvadan <flaviuvadan@gmail.com>
2020-07-05 18:26:26 -0600

    Change the Visualizer interface to expose a Process() rather than a separate N50 and L50

--- visualizer/interfaces.go
+++ visualizer/interfaces.go
@@ -4,9 +4,7 @@ type (
 	// Visualizer represents the interface that is used to interact with the visualizer package. It allows the
 	// construction of specific visualization that can be created after performing assembly and reporting steps
 	Visualizer interface {
-		// N50 creates the plots associated with reporting the N50 scores for assembly reports
-		N50() error
-		// L50 creates the plots associated with reporting the L50 scores for assembly reports
-		L50() error
+		// Process
+		Process() error
 	}
 )
