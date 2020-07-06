438ebd1921b4fe1906d6c3a1c58076dc44652af4
flaviuvadan <flaviuvadan@gmail.com>
2020-07-05 18:26:41 -0600

    Implement the Process() of the visualizer

--- visualizer/visualizer.go
+++ visualizer/visualizer.go
@@ -1,6 +1,12 @@
 package visualizer
 
-import "github.com/genomagic/reporter"
+import (
+	"os"
+
+	"github.com/go-echarts/go-echarts/charts"
+
+	"github.com/genomagic/reporter"
+)
 
 type (
 	// visualize implements the Visualizer interface for visualizing assembly reports
@@ -16,12 +22,31 @@ func NewVisualizer(reports []reporter.Report) Visualizer {
 	}
 }
 
-// N50 creates the plots associated with reporting the N50 scores for assembly reports
-func (v *visualize) N50() error {
-	return nil
-}
-
-// L50 creates the plots associated with reporting the N50 scores for assembly reports
-func (v *visualize) L50() error {
-	return nil
+// Process creates the plots associated with reporting the N50 and L50 scores for assembly reports
+func (v *visualize) Process() error {
+	var err error
+	names := make([]string, len(v.reports))
+	l50s := make([]int32, len(v.reports))
+	n50s := make([]int32, len(v.reports))
+	for i, n := range v.reports {
+		names[i] = n.AssemblyName
+		if l50s[i], err = n.GetL50(); err != nil {
+			return err
+		}
+		if n50s[i], err = n.GetN50(); err != nil {
+			return err
+		}
+	}
+	bar := charts.NewBar()
+	bar.SetGlobalOptions(charts.TitleOpts{
+		Title: "Assembly results",
+	})
+	bar.AddXAxis(names).
+		AddYAxis("N50", l50s).
+		AddYAxis("L50", n50s)
+	if f, err := os.Create("bar.html"); err != nil {
+		return err
+	} else {
+		return bar.Render(f)
+	}
 }
