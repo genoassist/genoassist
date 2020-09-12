a7187856d648e9dc45b3d09117c972648c6170dd
Tayab Soomro <isoomro.t@gmail.com>
2020-09-12 12:11:46 -0600

    patch the previous fix which incorrectly tackled the N50 issue. Fixes #102
    - add case which returns the length of the contig if it is the only contig present in the assembly.

--- reporter/N50.go
+++ reporter/N50.go
@@ -7,8 +7,15 @@ import (
 // getN50 computes and returns the N50 score of the Report contigs
 // https://en.wikipedia.org/wiki/N50,_L50,_and_related_statistics#N50
 func (r *Report) getN50() (int32, error) {
+
+	// immediately return the contig len if it is the only contig in the assembly
+	if len(r.result.Sequences) == 1 {
+		return int32(r.result.Sequences[0].Len()), nil
+	}
+
 	assemblyLen := 0
 	contigLens := make([]int, len(r.result.Sequences))
+
 	for i, seq := range r.result.Sequences {
 		contigLens[i] = seq.Len()
 		assemblyLen += seq.Len()
@@ -19,10 +26,11 @@ func (r *Report) getN50() (int32, error) {
 	}
 	sumToHalf := 0
 	for i, cl := range contigLens {
-		sumToHalf += cl
 		if sumToHalf >= halfAssemblyLen {
 			return int32(contigLens[i]), nil
 		}
+		sumToHalf += cl
+
 	}
 	return 0, fmt.Errorf("failed to compute N50 for an unknown reason")
 }
